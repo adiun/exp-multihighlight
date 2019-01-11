b66c418cb03187e0bdf4bfa0105206c2fecc899f
Adi Unnithan <adi.unnithan@gmail.com>
2019-01-11 09:54:09 -0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import LayersImage from "./layers.png";
 import LayerButton from "./layerButton";
 import "./styles.css";
 import Span from "./span";
